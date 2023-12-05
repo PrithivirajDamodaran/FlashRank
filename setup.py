0f042859c2fb062603569f97be39000bb439f7a6
Prithivi Da <prithivida@Prithividas-M1.local>
2023-12-05 21:42:17 +0700

    1st push

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FlashRank', 
+    version='0.1', 
+    packages=find_packages(),
+    install_requires=[
+        'tokenizers',
+        'onnxruntime',
+        'numpy',
+        'requests',
+        'tqdm'
+    ],  
+    author='Prithivi Da',
+    author_email='',
+    description='Ultra lite & Super fast SoTA cross-encoder based re-ranking for your search & retrieval pipelines.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PrithivirajDamodaran/FlashRank',  
+    license='Apache 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
