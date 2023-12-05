312869fd908ba5aba31e3f289d84aa797c3cdf3a
Prithivi Da <prithivida@Prithividas-M1.local>
2023-12-05 22:40:37 +0700

    Remove duplicate files outside of package directory

--- Config.py
+++
@@ -1,7 +0,0 @@
-model_url = 'https://storage.googleapis.com/flashrank/{}.zip'
-default_cache_dir = "/tmp"
-default_model = "ms-marco-TinyBERT-L-2-v2"
-model_file_map = {
-    "ms-marco-TinyBERT-L-2-v2": "flashrank-TinyBERT-L-2-v2.onnx",
-    "ms-marco-MiniLM-L-12-v2": "flashrank-MiniLM-L-12-v2.onnx",
-}
