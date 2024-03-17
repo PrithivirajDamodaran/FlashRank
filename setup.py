5e4d840fbbe6a599912cee0e1715b0e7037c397d
Prithivi Da <prithivida@Prithividas-M1.local>
2024-03-17 14:45:53 +0700

    config changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FlashRank', 
-    version='0.1.67', 
+    version='0.1.68', 
     packages=find_packages(),
     install_requires=[
         'tokenizers',
