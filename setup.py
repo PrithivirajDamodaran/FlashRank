834cd488f2bbbd245bf8cffbf507369c23bb72b6
Prithivi Da <Prithivi Da>
2024-06-19 18:24:04 -0700

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='FlashRank', 
-    version='0.2.5', 
+    version='0.2.6', 
     packages=find_packages(),
     install_requires=[
         'tokenizers',
